841f4bf35e7bdb592b97eb583dbc1237071f0f4a
ama266-ua <ama266@alu.ua.es>
2025-12-26 21:45:12 +0100

    Eliminar EnemyComponents.hpp y modificar ficheros afectados

--- src/ecs/systems/CollisionSystems.hpp
+++ src/ecs/systems/CollisionSystems.hpp
@@ -6,6 +6,6 @@
 #include "ecs/components/Player/PlayerInputComponent.hpp"
 #include "ecs/components/Player/PlayerStateComponent.hpp"
 #include "ecs/components/Player/PlayerStatsComponent.hpp"
-#include "ecs/components/EnemyComponents.hpp"
+#include "ecs/components/Enemy/EnemyIAComponent.hpp"
 
 void CollisionSystem(entt::registry &registry, Map &map);
