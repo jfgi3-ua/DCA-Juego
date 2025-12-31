a49e9fc450c8047af83ebaf7470a116baa0c8d7e
Fer Mint <jfgi3@alu.ua.es>
2025-12-31 13:43:12 +0100

    tests: limpia el test dummy ya obsoleto (#106)

--- tests/test_basic.cpp
+++
@@ -1,8 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-
-// Test básico para validar que Catch2 está integrado y CTest lo detecta.
-// En los siguientes pasos este archivo se ampliará con tests reales
-// puede que le cambie el nombre al archivo.
-TEST_CASE("Catch2 is wired up", "[smoke]") {
-    REQUIRE(1 + 1 == 2);
-}
