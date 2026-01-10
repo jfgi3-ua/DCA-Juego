5c32ba5a7ff5141f0405f0bf8bafbb21e7882906
ama266-ua <ama266@alu.ua.es>
2026-01-10 16:06:50 +0100

    Añadida las funciones para windows

--- src/core/Localization.cpp
+++ src/core/Localization.cpp
@@ -33,7 +33,14 @@ void InitLocalization(const std::string& lang) {
     bindtextdomain("messages", localeDir);
     textdomain("messages");
     bind_textdomain_codeset("messages", "UTF-8");
-    setenv("LANGUAGE", lang.c_str(), 1);
+    
+    #ifdef _WIN32 // Windows NO tiene setenv() 
+        SetEnvironmentVariableA("LANGUAGE", lang.c_str()); 
+        SetEnvironmentVariableA("LANG", lang.c_str()); 
+        SetEnvironmentVariableA("LC_MESSAGES", lang.c_str()); 
+    #else // Linux / macOS 
+        setenv("LANGUAGE", lang.c_str(), 1); 
+    #endif
 }
 
 void SwitchLocalization() {
